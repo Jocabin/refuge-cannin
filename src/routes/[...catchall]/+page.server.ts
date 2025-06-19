a509f33f1eef3b5a934066514b7a1da7a303a5ef
Jocabin <kobyfar@hotmail.com>
2025-06-19 14:31:51 +0200

    disable cache

--- src/routes/[...catchall]/+page.server.ts
+++ src/routes/[...catchall]/+page.server.ts
@@ -2,14 +2,17 @@ import { fetchEntries, fetchOneEntry, getBuilderSearchParams } from '@builder.io
 import { PUBLIC_BUILDER_IO_KEY } from '$env/static/public';
 
 export async function load(event) {
-  const content = await fetchOneEntry({
-    model: 'page',
-    apiKey: PUBLIC_BUILDER_IO_KEY,
-    options: getBuilderSearchParams(event.url.searchParams),
-    userAttributes: {
-      urlPath: event.url.pathname || '/',
-    },
-  });
+const content = await fetchOneEntry({
+        model: 'page',
+        apiKey: PUBLIC_BUILDER_IO_KEY,
+        options: {
+                ...getBuilderSearchParams(event.url.searchParams),
+                cachebust: true,
+        },
+        userAttributes: {
+                urlPath: event.url.pathname || '/',
+        },
+});
 
   const dogs = fetchEntries({
         model: 'chien',
