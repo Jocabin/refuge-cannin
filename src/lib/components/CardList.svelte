b14e5035cdac067a9c6c8d03e3a1c2056e38e224
Jocabin <kobyfar@hotmail.com>
2025-06-19 14:10:47 +0200

    add search

---
+++ src/lib/components/CardList.svelte
@@ -0,0 +1,46 @@
+<script>
+	// @ts-nocheck
+	let { results } = $props();
+</script>
+
+<div>
+	<p class="mb-4">{results.length} résultats</p>
+
+	<section class="dogGrid">
+		{#each results as result, i}
+			{@const r = result.data}
+
+			<article
+				class="bg-white w-full rounded-lg overflow-hidden flex flex-col"
+				class:firstOne={i == 0}
+			>
+				<div class="flex-1 w-full bg-red-50 overflow-hidden">
+					<img src={r.gallery[0].img} alt="" class="flex-1 w-full object-cover h-full" />
+				</div>
+				<div class="font-dm p-4">
+					<h2 class="font-bold text-3xl">{r.name}</h2>
+					<p>En attente de sa famille idéale</p>
+					<p>{r.sex}</p>
+					<p>
+						{new Date(r.birthdate).toLocaleDateString()} ({Math.floor(
+							(new Date() - new Date(r.birthdate)) / (1000 * 60 * 60 * 24 * 365)
+						)} ans)
+					</p>
+				</div>
+			</article>
+		{/each}
+	</section>
+</div>
+
+<style>
+	.dogGrid {
+		display: grid;
+		gap: 1rem;
+		grid-template-columns: repeat(auto-fill, minmax(375px, 1fr));
+		grid-template-rows: repeat(auto-fill, 350px);
+	}
+
+	.firstOne {
+		grid-row: 1 / 3;
+	}
+</style>
---
+++ src/lib/select-datas.js
@@ -0,0 +1,18 @@
+export const environment = [
+        { value: '', text: 'Ton environnement' },
+        { value: 'ville', text: 'Ville' },
+        { value: 'campagne', text: 'Campagne' }
+];
+export const lifestyle = [
+        { value: '', text: 'Ton quotidien' },
+        { value: 'work', text: 'Travail en journée, dispo matin/soir' },
+        { value: 'atHouse', text: 'Présence quotidienne à la maison' },
+        { value: 'goOut', text: 'Routine stable avec sorties régulières' }
+];
+export const entourage = [
+        { value: '', text: 'Ton entourage' },
+        { value: 'solo', text: 'Personne seule' },
+        { value: 'noChilds', text: 'Couple sans enfant' },
+        { value: 'otherDog', text: 'Avec un autre chien sociable' },
+        { value: 'familia', text: 'Famille avec enfants' }
+];
