383e74322aa155d427a434809dd96055209ceb53
Gabriel BREHAULT <gabrielbrehault@MacBook-Pro-de-Gabriel.local>
2025-06-20 09:50:52 +0200

    modif woff igor je te vois encore

--- src/lib/components/adopter.svelte
+++ src/lib/components/adopter.svelte
@@ -32,19 +32,19 @@
 						</li>
 					</ul>
 				{/if}
-				{#if ctaText && ctaText.trim() !== ''}
-					<a
-						href={ctaLink}
-						class="bg-white border-white border-2 py-[5px] px-[32px] rounded-4xl font-dm transition-all duration-300 hover:bg-transparent hover:border-2 hover:border-white"
-						>{ctaText}
-					</a>
-				{/if}
-				<p
-					class="absolute left-80 top-120 font-just text-[41px] font-normal leading-[30px] rotate-10"
-				>
-					{woff1} <br />
-					{woff2}
-				</p>
+				<div class="flex gap-10 justify-center items-center">
+					{#if ctaText && ctaText.trim() !== ''}
+						<a
+							href={ctaLink}
+							class="bg-white border-white border-2 py-[5px] px-[32px] rounded-4xl font-dm transition-all duration-300 hover:bg-transparent hover:border-2 hover:border-white"
+							>{ctaText}
+						</a>
+					{/if}
+					<p class="font-just text-[41px] font-normal leading-[30px] rotate-10">
+						{woff1} <br />
+						{woff2}
+					</p>
+				</div>
 			</div>
 			<div>
 				<img class="" src={img} width="380" alt="" />
